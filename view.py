53090611b754fbd8a5b660fe1b7cf500013d19ce
ZackReee <70840972+ZackReee@users.noreply.github.com>
2021-04-21 20:50:59 +0800

    Update view.py
    Created an entry field

--- view.py
+++ view.py
@@ -2,5 +2,10 @@ import tkinter as tk
 
 window = tk.Tk()
 
-# empty = tk.Label(text="Your diary seems empty")
-# empty.pack
+empty = tk.Label(text="Your diary seems empty")
+empty.pack()
+
+text_entry = tk.Text()
+text_entry.pack()
+
+tk.mainloop()
